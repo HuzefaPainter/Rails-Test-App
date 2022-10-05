a71427049d9ef8fd528bc99de5214686396ee7a7
HuzPainter <huzefa@intemlabs.com>
2022-10-06 03:54:52 +0530

    bootstrap-ified views

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_184848) do
+ActiveRecord::Schema.define(version: 2022_10_05_211748) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.time "created_at"
-    t.time "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
